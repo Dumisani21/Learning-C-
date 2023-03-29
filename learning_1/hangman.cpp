857bc9163443f99ad0abe0fd3a1e52f012c88671
Dumisani Mbonani <dumbona022@student.wethinkcode.co.za>
2023-03-29 12:27:34 +0200

    will fix this code later

--- learning_1/hangman.cpp
+++ learning_1/hangman.cpp
@@ -9,7 +9,25 @@ using std::endl;
 
 string muskWord(std::random_device& rd ,const string& word);
 int randomNum(std::random_device& rd, const int& length);
-bool checkPosition(const string& guess, const string& answer);
+bool updateGuess(string& guess, const string& answer, const char& guessChar);
+void print(const string& message);
+void printl(const string& message);
+char input();
+
+void print(const string& message) {
+  std::cout << message;
+}
+
+void printl(const string& message) {
+  std::cout << message << "\n";
+}
+
+char input(){
+    char data;
+    std::cin.get(data);
+    std::cin.ignore(100, '\n'); // Ignore any remaining input on the line
+    return data;
+}
 
 string muskWord(std::random_device& rd ,const string& word) {
     string newWord = word;  
@@ -27,13 +45,32 @@ int randomNum(std::random_device& rd, const int& length) {
     return dist(rd);
 }
 
+bool updateGuess(string& guess, const string& answer, const char& guessChar) {
+    for (int i = 0; i < answer.length(); i++) {
+        if (guessChar == answer[i] && guessChar != guess[i]) {
+            guess[i] = guessChar;
+            return true;
+        }
+    }
+    return false;
+}
+
+
 int main() {
     std::random_device rd;
     std::vector<string> words = {"Google", "Jump", "People", "Run", "Know"};
     int length = words.size();
+    int limit = 12;
     int ranWordIndex = randomNum(rd, length-1);
     string guess = muskWord(rd, words[ranWordIndex]);
-    cout << guess << endl;
+    
+    printl("Your guess is :" + guess);
+    while (true) {
+        print("Enter guess: ");
+        char guessLetter = input();
+        
+        bool result = updateGuess(guess, )
+    }
 
     return 0;
 }
