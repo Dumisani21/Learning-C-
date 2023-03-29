7805c89d6a480b98a1a4db72a63cdcf1c68af8e2
Dumisani Mbonani <dumbona022@student.wethinkcode.co.za>
2023-03-29 09:33:55 +0200

    C++ ChatGPT implemented the use of vertors and not arrays

--- learning_1/app.cpp
+++ learning_1/app.cpp
@@ -1,57 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <random>
 
 typedef std::string String;
 
-int randomGenerator(int length);
-void print(String message);
-void printl(String message);
+int randomGenerator(std::random_device& rd, int length);
+void print(const String& message);
+void printl(const String& message);
 String input();
-String getInput(String message);
+String getInput(const String& message);
 
-int randomGenerator(int length) {
-  std::random_device rd;
+int randomGenerator(std::random_device& rd, int length) {
   std::uniform_int_distribution<int> dist(0, length);
   return dist(rd);
 }
 
-void print(String message) {
+void print(const String& message) {
   std::cout << message;
 }
 
-void printl(String message) {
+void printl(const String& message) {
   std::cout << message << "\n";
 }
 
 String input(){
   String data;
-  std::cin >> data;
+  std::getline(std::cin, data);
   return data;
 }
 
-String getInput(String message) {
+String getInput(const String& message) {
   print(message);
-  String output = input();
-  return output;
+  return input();
 }
 
-
 int main() {
-
-  std::string words[] = {"people", "jump", "run", "Food", "Game"};
-  int length = sizeof(words)/sizeof(words[0]);
-  int getRandom = randomGenerator(length-1);
+  std::random_device rd;
+  std::vector<String> words = {"people", "jump", "run", "Food", "Game"};
+  int length = words.size();
+  int getRandom = randomGenerator(rd, length-1);
 
   while (true) {
-    print("Enter command: ");
-    String cmd = input();
+    String cmd = getInput("Enter command: ");
     std::cout << "You entered: " + cmd << "\n";
     if (cmd == "exit"){
       printl("Goodbye!");
       break;
     }
   }
-
 
   return 0;
 }
